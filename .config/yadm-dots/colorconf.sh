b6e7216885c88190ccf1f3f35b7f0f7b63f8d444
Derek Jiang <derek.l.jiang@gmail.com>
2022-12-09 15:50:40 -0800

    colorscheme conf?

--- .config/yadm-dots/colorconf.sh
+++ .config/yadm-dots/colorconf.sh
@@ -1,4 +1,32 @@
 #!/bin/sh
-#
-export RED=#EE8888
-export RED_HI=#FF5555
+# THIS IS THE BASE COLOR FILE: ALL OTHER TEMPLATES DERIVE FROM THIS
+# Exports env variables so yadm can fill them into dotfiles with ##template.default
+# TO USE: run `source colorconf.sh`, then `yadm alt`
+
+export COLOR_FG=#cec7b1
+export COLOR_BG=#232b2f
+# Color 	(Normal, Bright)
+# Black 	(0,  8)
+export BLACK=#13273c
+export BLACK_HI=#49596f
+# Red 		(1,  9)
+export RED=#a54242
+export RED_HI=#cc6666
+# Green 	(2, 10)
+export GREEN=#8c9440
+export GREEN_HI=#b5bd68
+# Yellow	(3, 11)
+export YELLOW=#de935f
+export YELLOW_HI=#f0c674
+# Blue		(4, 12)
+export BLUE=#5f819d
+export BLUE_HI=#81a2be
+# Purple	(5, 13)
+export PURPLE=#85678f
+export PURPLE_HI=#b294bb
+# Cyan		(6, 14)
+export CYAN=#5e8d87
+export CYAN_HI=#8abeb7
+# White		(7, 15)
+export WHITE=#707880
+export WHITE_HI=#c5c8c6
--- Documents/yadm-dots/firefox/README.md
+++
@@ -1,6 +0,0 @@
-# Why are these files here?
-FF needs to be manually configured since the profile directory name changes between installs. 
-1: go to about:support
-2: find "Profile Directory", open it
-3: add a directory named "chrome"
-4: add "userChrome.css" to the directory
