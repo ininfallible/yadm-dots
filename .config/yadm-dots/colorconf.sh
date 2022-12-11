c63ed0251b5e9065ade8461ecba6ece3f11a0e22
Derek Jiang <derek.l.jiang@gmail.com>
2022-12-11 04:49:37 -0800

    gtk sway waybar confs

--- .config/yadm-dots/colorconf.sh
+++ .config/yadm-dots/colorconf.sh
@@ -3,12 +3,12 @@
 # Exports env variables so yadm can fill them into dotfiles with ##template.default
 # TO USE: run `source colorconf.sh`, then `yadm alt`
 
-export COLOR_FG=d9cfaf
+export COLOR_FG=c3b897
 export COLOR_BG=353830
 # Color 	(Normal, Bright)
 # Black 	(0,  8)
-export BLACK=13273c
-export BLACK_HI=49596f
+export BLACK=343625
+export BLACK_HI=686c52
 # Red 		(1,  9)
 export RED=a54242
 export RED_HI=cc6666
@@ -16,8 +16,8 @@ export RED_HI=cc6666
 export GREEN=8c9440
 export GREEN_HI=b5bd68
 # Yellow	(3, 11)
-export YELLOW=de935f
-export YELLOW_HI=f0c674
+export YELLOW=c6a167
+export YELLOW_HI=f3d496
 # Blue		(4, 12)
 export BLUE=5f819d
 export BLUE_HI=81a2be
@@ -28,5 +28,5 @@ export PURPLE_HI=b294bb
 export CYAN=5e8d87
 export CYAN_HI=8abeb7
 # White		(7, 15)
-export WHITE=707880
-export WHITE_HI=c5c8c6
+export WHITE=aa9c7e
+export WHITE_HI=d2c1ac
