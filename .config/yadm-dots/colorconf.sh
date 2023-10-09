bb413a6dfae8bb115d0ec3a94a3f04170a5df826
Derek Jiang <derek.l.jiang@gmail.com>
2023-10-09 01:30:59 -0700

    new colorscheme

--- .config/yadm-dots/colorconf.sh
+++ .config/yadm-dots/colorconf.sh
@@ -3,12 +3,15 @@
 # Exports env variables so yadm can fill them into dotfiles with ##template.default
 # TO USE: run `source colorconf.sh`, then `yadm alt`
 
-export COLOR_FG=c3b897
-export COLOR_BG=353830
+export COLOR_FG=cac2b1
+export COLOR_BG=2d3232
+export COLOR_ACCENT=c87f56
+export COLOR_ACCENT_HI=dba26e
+
 # Color 	(Normal, Bright)
 # Black 	(0,  8)
-export BLACK=343625
-export BLACK_HI=686c52
+export BLACK=2c333e
+export BLACK_HI=52656c
 # Red 		(1,  9)
 export RED=a54242
 export RED_HI=cc6666
@@ -16,8 +19,8 @@ export RED_HI=cc6666
 export GREEN=8c9440
 export GREEN_HI=b5bd68
 # Yellow	(3, 11)
-export YELLOW=c6a167
-export YELLOW_HI=f3d496
+export YELLOW=c88d56
+export YELLOW_HI=e1b57d
 # Blue		(4, 12)
 export BLUE=5f819d
 export BLUE_HI=81a2be
@@ -28,5 +31,5 @@ export PURPLE_HI=b294bb
 export CYAN=5e8d87
 export CYAN_HI=8abeb7
 # White		(7, 15)
-export WHITE=aa9c7e
-export WHITE_HI=d2c1ac
+export WHITE=879382
+export WHITE_HI=bcc2b3
